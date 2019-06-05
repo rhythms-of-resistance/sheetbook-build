e5711c3bc53a62b3081b54df424b39863e9b0ab6
Candid Dauth <cdauth@cdauth.eu>
2019-06-05 12:53:55 +0200

    Initial commit

---
+++ build.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+if [ ! -f /home/ror/sheetbook/make-sheets.sh ]; then
+    echo "Mount sheetbook in /home/ror/sheetbook"
+    exit 1
+fi
+
+cd /home/ror/sheetbook
+
+rm -rf generated
+mkdir generated
+chown ror:ror generated
+su ror -c ./make-sheets.sh
+chown -R "$(stat -c %u:%g make-sheets.sh)" generated
